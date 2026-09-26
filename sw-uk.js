b966a29a53d98ca3d053b55ad2baf359b25d6608
jeevanathwal96 <jeevanathwal96@gmail.com>
2026-09-26 15:26:42 +0100

    tax formatting correction

--- sw-uk.js
+++ sw-uk.js
@@ -11,7 +11,7 @@
  */
 'use strict';
 
-var APP_VERSION = 'uk-2026-09-26-373';
+var APP_VERSION = 'uk-2026-09-26-374';
 var CACHE = 'agriinsights-uk-' + APP_VERSION;
 
 /* App shell precached on install. The ?v=-suffixed JS is intentionally left to
