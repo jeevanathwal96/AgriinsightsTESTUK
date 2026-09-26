f9ddd68cfa4090e3c60ef19016c165f94010ff60
jeevanathwal96 <jeevanathwal96@gmail.com>
2026-09-26 08:17:18 +0100

    bug ifxes

--- sw-uk.js
+++ sw-uk.js
@@ -11,7 +11,7 @@
  */
 'use strict';
 
-var APP_VERSION = 'uk-2026-09-25-371';
+var APP_VERSION = 'uk-2026-09-26-372';
 var CACHE = 'agriinsights-uk-' + APP_VERSION;
 
 /* App shell precached on install. The ?v=-suffixed JS is intentionally left to
